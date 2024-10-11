9efe7e195d02164d42a51a254b76ba0bd82c0962
Abdul Rehman <ar618211@gmail.com>
2024-10-11 13:07:12 +0500

    removed errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "@/components/sections/header";
 import Footer from "@/components/sections/footer";
-import ErrorButton from "@/components/elements/error-button";
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <Header showCTAButton />
       <main className="flex-grow flex flex-col justify-center items-center">
         <h2 className="font-bold text-2xl">Bunch of SEO filled Landing Page here</h2>
-        <ErrorButton />
       </main>
       <Footer />
     </div>
